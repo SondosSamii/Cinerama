aa487909d842d3cccac3a2f3cef4a09b4e531b1a
SondosSamii <sondos.sami.abuzeid@gmail.com>
2020-07-12 19:05:26 +0200

    git commit -m "Edit API Key"

--- src/app/movies.service.ts
+++ src/app/movies.service.ts
@@ -1,45 +1,59 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class MoviesService {
+  constructor(public _HttpClient: HttpClient) {}
+  apiKey = "def5b6d9eea61749d3aac420b0f7ddeb";
 
-  constructor(public _HttpClient:HttpClient) {
-    
+  getTrendingAll(): Observable<any> {
+    return this._HttpClient.get(
+      `https://api.themoviedb.org/3/trending/all/day?api_key=${this.apiKey}`
+    );
   }
 
-  getTrendingAll():Observable<any> {
-    return this._HttpClient.get("https://api.themoviedb.org/3/trending/all/day?api_key=def5b6d9eea61749d3aac420b0f7ddeb");
+  getTrendingMovies(): Observable<any> {
+    return this._HttpClient.get(
+      `https://api.themoviedb.org/3/trending/movies/day?api_key=${this.apiKey}`
+    );
   }
 
-  getTrendingMovies():Observable<any> {
-    return this._HttpClient.get("https://api.themoviedb.org/3/trending/movies/day?api_key=def5b6d9eea61749d3aac420b0f7ddeb");
+  getTrendingTV(): Observable<any> {
+    return this._HttpClient.get(
+      `https://api.themoviedb.org/3/trending/tv/day?api_key=${this.apiKey}`
+    );
   }
 
-  getTrendingTV():Observable<any> {
-    return this._HttpClient.get("https://api.themoviedb.org/3/trending/tv/day?api_key=def5b6d9eea61749d3aac420b0f7ddeb");
+  getMovieDetails(id): Observable<any> {
+    return this._HttpClient.get(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=${this.apiKey}`
+    );
   }
 
-  getMovieDetails(id):Observable<any> {
-    return this._HttpClient.get(`https://api.themoviedb.org/3/movie/${id}?api_key=def5b6d9eea61749d3aac420b0f7ddeb`);
+  getTVDetails(id): Observable<any> {
+    return this._HttpClient.get(
+      `https://api.themoviedb.org/3/tv/${id}?api_key=${this.apiKey}&language=en-US`
+    );
   }
 
-  getTVDetails(id):Observable<any> {
-    return this._HttpClient.get(`https://api.themoviedb.org/3/tv/${id}?api_key=def5b6d9eea61749d3aac420b0f7ddeb&language=en-US`);
+  getNowPlayingMovies(): Observable<any> {
+    return this._HttpClient.get(
+      `https://api.themoviedb.org/3/movie/now_playing?api_key=${this.apiKey}&language=en-US&page=1`
+    );
   }
 
-  getNowPlayingMovies():Observable<any> {
-    return this._HttpClient.get("https://api.themoviedb.org/3/movie/now_playing?api_key=def5b6d9eea61749d3aac420b0f7ddeb&language=en-US&page=1");
+  getLatestMovie(): Observable<any> {
+    return this._HttpClient.get(
+      `https://api.themoviedb.org/3/movie/latest?api_key=${this.apiKey}&language=en-US`
+    );
   }
 
-  getLatestMovie():Observable<any> {
-    return this._HttpClient.get("https://api.themoviedb.org/3/movie/latest?api_key=def5b6d9eea61749d3aac420b0f7ddeb&language=en-US");
-  }
-
-  getLatestTV():Observable<any> {
-    return this._HttpClient.get("https://api.themoviedb.org/3/tv/latest?api_key=def5b6d9eea61749d3aac420b0f7ddeb&language=en-US");
+  getLatestTV(): Observable<any> {
+    return this._HttpClient.get(
+      `https://api.themoviedb.org/3/tv/latest?api_key=${this.apiKey}&language=en-US`
+    );
   }
 }
