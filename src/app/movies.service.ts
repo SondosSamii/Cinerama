311205f6e0f41a01a007c1d80e046fb6b3f3b406
SondosSamii <sondos.sami.abuzeid@gmail.com>
2020-07-12 20:36:15 +0200

    Update lodash to @4.17.19 and yargs-parser to @15.0.1

--- src/app/movies.service.ts
+++ src/app/movies.service.ts
@@ -8,54 +8,50 @@ import { Observable } from "rxjs";
 export class MoviesService {
   constructor(public _HttpClient: HttpClient) {}
   baseUrl = "https://api.themoviedb.org/3/";
-  apiKey = "def5b6d9eea61749d3aac420b0f7ddeb";
+  apiKey = "?api_key=def5b6d9eea61749d3aac420b0f7ddeb";
   lang = "&language=en-US";
 
   getTrendingAll(): Observable<any> {
     return this._HttpClient.get(
-      `${this.baseUrl}trending/all/day?api_key=${this.apiKey}`
+      `${this.baseUrl}trending/all/day${this.apiKey}`
     );
   }
 
   getTrendingMovies(): Observable<any> {
     return this._HttpClient.get(
-      `${this.baseUrl}trending/movies/day?api_key=${this.apiKey}`
+      `${this.baseUrl}trending/movies/day${this.apiKey}`
     );
   }
 
   getTrendingTV(): Observable<any> {
-    return this._HttpClient.get(
-      `${this.baseUrl}trending/tv/day?api_key=${this.apiKey}`
-    );
+    return this._HttpClient.get(`${this.baseUrl}trending/tv/day${this.apiKey}`);
   }
 
   getMovieDetails(id): Observable<any> {
-    return this._HttpClient.get(
-      `${this.baseUrl}movie/${id}?api_key=${this.apiKey}`
-    );
+    return this._HttpClient.get(`${this.baseUrl}movie/${id}${this.apiKey}`);
   }
 
   getTVDetails(id): Observable<any> {
     return this._HttpClient.get(
-      `${this.baseUrl}tv/${id}?api_key=${this.apiKey}${this.lang}`
+      `${this.baseUrl}tv/${id}${this.apiKey}${this.lang}`
     );
   }
 
   getNowPlayingMovies(): Observable<any> {
     return this._HttpClient.get(
-      `${this.baseUrl}movie/now_playing?api_key=${this.apiKey}${this.lang}`
+      `${this.baseUrl}movie/now_playing${this.apiKey}${this.lang}`
     );
   }
 
   getLatestMovie(): Observable<any> {
     return this._HttpClient.get(
-      `${this.baseUrl}movie/latest?api_key=${this.apiKey}${this.lang}`
+      `${this.baseUrl}movie/latest${this.apiKey}${this.lang}`
     );
   }
 
   getLatestTV(): Observable<any> {
     return this._HttpClient.get(
-      `${this.baseUrl}tv/latest?api_key=${this.apiKey}${this.lang}`
+      `${this.baseUrl}tv/latest${this.apiKey}${this.lang}`
     );
   }
 }
