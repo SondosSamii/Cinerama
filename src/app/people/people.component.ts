2538a65facad983b782eb3f2c1c1d5236227d5e3
Sondos Sami <sondos.sami.abuzeid@gmail.com>
2024-01-02 11:25:47 +0200

    chore: add comments to ngOnInit, remove unused constructor, and format the files

--- src/app/people/people.component.ts
+++ src/app/people/people.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-people',
-  templateUrl: './people.component.html',
-  styleUrls: ['./people.component.scss']
+  selector: "app-people",
+  templateUrl: "./people.component.html",
+  styleUrls: ["./people.component.scss"],
 })
 export class PeopleComponent implements OnInit {
-
-  constructor() { }
-
   ngOnInit() {
+    // Remove the empty ngOnInit if not needed
   }
-
 }
--- src/app/tv/tv.component.ts
+++ src/app/tv/tv.component.ts
@@ -1,21 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { MoviesService } from '../movies.service';
+import { Component, OnInit } from "@angular/core";
+import { MoviesService } from "../movies.service";
 
 @Component({
-  selector: 'app-tv',
-  templateUrl: './tv.component.html',
-  styleUrls: ['./tv.component.scss']
+  selector: "app-tv",
+  templateUrl: "./tv.component.html",
+  styleUrls: ["./tv.component.scss"],
 })
 export class TvComponent implements OnInit {
-
-  public trendingTV:any[];
-  constructor(public _MoviesService:MoviesService ) {
-    _MoviesService.getTrendingTV().subscribe(data => {
+  public trendingTV: any[];
+  constructor(public _MoviesService: MoviesService) {
+    _MoviesService.getTrendingTV().subscribe((data) => {
       this.trendingTV = data.results;
     });
   }
 
   ngOnInit() {
+    // Remove the empty ngOnInit if not needed
   }
-
 }
